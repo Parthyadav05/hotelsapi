60d3036ac2e990e5fac99a5cb5ca81ffd2cb387b
Parth Yadav <parthyadav9368166097@gmail.com>
2025-03-18 00:55:23 +0530

    added cloud  database

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 // Define the MongoDB database URL
-const mongoUrl = 'mongodb://localhost:27017/hotel';
-
+//const mongoUrl = 'mongodb://localhost:27017/hotel';
+//const mongoUrl = process.env.LOCAL_DB_URL;
+const mongoUrl  = process.env.DBURL;
 mongoose.connect(mongoUrl, {
     useNewUrlParser: true,  // ✅ Corrected option
     useUnifiedTopology: true
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const menuRouter = require('./routers/menuRouter');
 const db = require('./db'); // MongoDB connection
- // Capitalized model
+require('dotenv').config();
 const Menu = require('./models/menu'); // Capitalized model
 const personRouter = require('./routers/personRouter');
 
@@ -12,7 +12,7 @@ const app = express();
 // ✅ Correct order of middleware
 app.use(express.json()); // Correct way to parse JSON
 app.use(express.urlencoded({ extended: true })); 
-
+const PORT = process.env.PORT || 3000;
 // ✅ Welcome Route
 app.get('/', (req, res) => {
     res.send('Welcome to the World of API');
@@ -21,6 +21,7 @@ app.use('/person' , personRouter);
 app.use('/menu' , menuRouter);
 
 // ✅ Start Server
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 });
